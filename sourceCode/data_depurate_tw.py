5994910e9269389dea3d2129f7202eff8a3364c1
mvelasco93 <m_velasco93@hotmail.com>
2016-06-25 00:40:55 -0500

    Data depuration

--- sourceCode/data_depurate_tw.py
+++ sourceCode/data_depurate_tw.py
@@ -1,29 +1,52 @@
-__author__ = 'cesar17'
+# -*- coding: utf-8 -*-
+
+__author__ = 'madevelasco'
 
 import json
 import re
+from nltk.corpus import stopwords
 
 def get_fields_tw():
-    tw_file = open('../data/tweets_file.csv', 'w')
-    cabecera = 'timestamp_ms,userId,longitude,latitude,texto,language,favorite_count\n'
+    tw_file = open('../data/Twitter/tweets_file.csv', 'w')
+    cabecera = 'id_str,userId,longitude,latitude,words, lang, timestamp_ms \n'
     tw_file.write(cabecera)
-    data = open('text.json')
+    data = open('tweets160616.json')
     # Cada line es un objeto json
     for line in data:
-        tweet = json.loads(line)
-        longitude = str(tweet['coordinates']['coordinates'][0])     # lo que esta entre llaves representa el key y lo que retorna es el value  K
-        latitude = str(tweet['coordinates']['coordinates'][1])
-        texto = tweet['text']
-        texto = re.sub(r'@(.+?)\s+', '', texto)
-        texto = re.sub(r'#(.+?)\s+', '', texto)
-        texto = re.sub(r'http(.+?)\s+', '', texto)
-        texto = re.sub(r'http(.+?)\Z', '', texto)
-        print texto
-        print '----'
-        line = tweet['id_str'] + ',' + longitude + ',' + latitude + ',' + texto + ',' + tweet['created_at'] + '\n'
-        #print line
+        if (line.strip() != '\n'):
+            try:
+                tweet = json.loads(line)
+                longitude = str(tweet['coordinates']['coordinates'][0])
+                latitude = str(tweet['coordinates']['coordinates'][1])
+                texto = tweet['text']
+                texto = re.sub(r'@(.+?)\s+', '', texto)
+                texto = re.sub(r'#(.+?)\s+', '', texto)
+                texto = re.sub(r'http(.+?)\s+', '', texto)
+                texto = re.sub(r'http(.+?)\Z', '', texto)
+                content = normalization(texto, tweet['lang'])
+                #No devuelve tweet si está vacío tras normalizar y eliminar stopwords
+                if (content != ""):
+                    line = tweet['id_str'] + ',' + str(tweet['user']['id'])+','+ longitude + ',' \
+                           + latitude + ',' + content + ',' +tweet['lang']+','+ tweet['timestamp_ms'] + '\n'
+                    tw_file.write(line)
+            except ValueError:
+                print 'Decoding JSON has failed'
     tw_file.close()
     data.close()
+
+def normalization(text, lang):
+    text = text.lower()
+    words = re.findall(r'\w+', text, flags=re.UNICODE | re.LOCALE)
+    depured = ""
+    for word in words:
+        #Depuración de acuerdo al idioma
+        if lang == "en":
+            if word not in stopwords.words('english'):
+                depured = depured+"&"+word
+        else:
+            if word not in stopwords.words('spanish'):
+                depured = depured + "&" + word
+    return depured
 
 if __name__ == '__main__':
     get_fields_tw()
