5026715a42d0ba6e164e72d591417cec4ae086a3
Cesar San Lucas <slace.93@gmail.com>
2016-07-05 10:20:35 -0500

    Agregado stemming

--- sourceCode/data_depurate_tw.py
+++ sourceCode/data_depurate_tw.py
@@ -5,33 +5,55 @@ __author__ = 'madevelasco'
 import json
 import re
 import unicodedata
+import hunspell
 from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+
 
 def get_fields_tw():
     tw_file = open('../data/Twitter/tweets_file.csv', 'w')
-    cabecera = 'id_str,userId,longitude,latitude,words, lang, timestamp_ms \n'
+    tw_file_no_text = open('../data/Twitter/tweets_noText.csv', 'w')
+    cabecera = 'id_str,longitude,latitude,words,lang,timestamp_ms,n_retw,hashtags\n'
+    cabecera_no_text = 'id_str,longitude,latitude,lang,timestamstamp_ms,n_retw\n'
     tw_file.write(cabecera)
+    tw_file_no_text.write(cabecera_no_text)
     #Reemplazar por nombre archivo
-    data = open('tweets170616.json')
+    data = open('text.json')
     # Cada line es un objeto json
     for line in data:
         line = line
         if (line.strip() != '\n'):
+            hashtags = ''
             try:
                 tweet = json.loads(line)
-                longitude = str(tweet['coordinates']['coordinates'][0])
-                latitude = str(tweet['coordinates']['coordinates'][1])
-                texto = tweet['text']
-                texto = re.sub(r'@(.+?)\s+', '', texto)
-                texto = re.sub(r'#(.+?)\s+', '', texto)
-                texto = re.sub(r'http(.+?)\s+', '', texto)
-                texto = re.sub(r'http(.+?)\Z', '', texto)
-                content = normalization(texto, tweet['lang'])
-                #No devuelve tweet si está vacío tras normalizar y eliminar stopwords
-                if (content != ""):
-                    line = tweet['id_str'] + ',' + str(tweet['user']['id'])+','+ longitude + ',' \
-                           + latitude + ',' + content + ',' +tweet['lang']+','+ tweet['timestamp_ms'] + '\n'
-                    tw_file.write(line)
+                if tweet['coordinates']:
+                    longitude = str(tweet['coordinates']['coordinates'][0])
+                    latitude = str(tweet['coordinates']['coordinates'][1])
+                    texto = tweet['text']
+                    texto = re.sub(r'@(.+?)\s+', '', texto)
+                    texto = re.sub(r'#(.+?)\s+', '', texto)
+                    texto = re.sub(r'http(.+?)\s+', '', texto)
+                    texto = re.sub(r'http(.+?)\Z', '', texto)
+                    texto = re.sub(r'jaja{1,}?', '', texto)
+                    texto = re.sub(r'j{2,}?', '', texto)
+                    texto = re.sub(r'jeje{1,}?', '', texto)
+                    content = normalization(texto, tweet['lang'])
+                    #No devuelve tweet si está vacío tras normalizar y eliminar stopwords
+                    if (content != ""):
+                        for h in tweet['entities']['hashtags']:
+                            hashtags = hashtags + '&' + h['text'].encode('ascii','ignore')
+                        if not hashtags:
+                            hashtags = '-'
+                        line = tweet['id_str'] + ',' + longitude + ',' \
+                               + latitude + ',' + content + ',' + tweet['lang']+','+ tweet['timestamp_ms']\
+                               + ',' + str(tweet['retweet_count']) + ',' + hashtags + '\n'
+                        tw_file.write(line)
+                    line_noText = tweet['id_str'] + ',' + longitude + ',' \
+                               + latitude + ',' + tweet['lang']+','+ tweet['timestamp_ms']\
+                               + ',' + str(tweet['retweet_count']) + '\n'
+                    tw_file_no_text.write(line_noText)
+                else:
+                    print tweet['coordinates']
             except ValueError:
                 print 'Decoding JSON has failed'
     tw_file.close()
@@ -40,6 +62,8 @@ def get_fields_tw():
 def normalization(text, lang):
     text = text.lower()
     other = ''
+    hobj_spanish = hunspell.HunSpell('dic_spanish/Spanish.dic', 'dic_spanish/Spanish.aff')
+    stemmer_english = SnowballStemmer("english")
     for element in text:
         other =other+remove_accents(element)
     text = other
@@ -50,9 +74,13 @@ def normalization(text, lang):
         #Depuración de acuerdo al idioma
         if lang == "en":
             if word not in stopwords.words('english'):
+                word = stemmer_english.stem(word)
                 depured = depured+"&"+word
         else:
             if word not in stopwords.words('spanish'):
+                stem = hobj_spanish.stem(word)
+                if stem:
+                    word = stem[0]
                 depured = depured + "&" + word
 
     return depured
