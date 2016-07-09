9a1f2425c6f2552310dbcc73a09339021e8bf820
mvelasco93 <m_velasco93@hotmail.com>
2016-07-09 18:02:20 -0500

    data depuration
    remove extra duplicated characters (ex: 'wenoooo'), pass stopwords by
    english and spanish, added time information, don't consider words if it
    is less than two characters

--- sourceCode/data_depurate_tw.py
+++ sourceCode/data_depurate_tw.py
@@ -9,13 +9,14 @@ import hunspell
 import sys
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
+import datetime
 
 
 def get_fields_tw():
     tw_file = open('../data/Twitter/tweets_depurated.csv', 'w')
     tw_file_no_text = open('../data/Twitter/tweets_depurated_noText.csv', 'w')
-    cabecera = 'id_str,longitude,latitude,words,lang,timestamp_ms,n_retw,hashtags\n'
-    cabecera_no_text = 'id_str,longitude,latitude,lang,timestamstamp_ms,n_retw\n'
+    cabecera = 'id_str,longitude,latitude,words,lang,timestamp_ms,n_retw,hashtags,dia,hora,isDay,isWeekDay\n'
+    cabecera_no_text = 'id_str,longitude,latitude,lang,timestamstamp_ms,n_retw,dia,hora,isDay,isWeekDay\n'
     tw_file.write(cabecera)
     tw_file_no_text.write(cabecera_no_text)
     #Reemplazar por nombre archivo
@@ -40,27 +41,36 @@ def get_fields_tw():
                     texto = re.sub(r'jaja{1,}?', '', texto)
                     texto = re.sub(r'j{2,}?', '', texto)
                     texto = re.sub(r'jeje{1,}?', '', texto)
-                    content = normalization(texto, tweet['lang'])
+                    content = normalization(texto)
+
+                    #Time information
+                    esdia = isDay(tweet['timestamp_ms'])
+                    entresemana = isWeekDay(tweet['timestamp_ms'])
+                    dia = day(tweet['timestamp_ms'])
+                    hora = hour(tweet['timestamp_ms'])
+
                     #No devuelve tweet si está vacío tras normalizar y eliminar stopwords
-                    if (content != "" and len(content)>=3):
+                    if (content != "" ):
                         for h in tweet['entities']['hashtags']:
                             hashtags = hashtags + '&' + h['text'].encode('ascii','ignore')
                         if not hashtags:
                             hashtags = '-'
                         line = tweet['id_str'] + ',' + longitude + ',' \
                                + latitude + ',' + content + ',' + tweet['lang']+','+ tweet['timestamp_ms']\
-                               + ',' + str(tweet['retweet_count']) + ',' + hashtags + '\n'
+                               + ',' + str(tweet['retweet_count']) + ',' + hashtags + ','+dia+','\
+                               +hora+','+esdia+','+entresemana+','+'\n'
                         tw_file.write(line)
+
                     line_noText = tweet['id_str'] + ',' + longitude + ',' \
                                + latitude + ',' + tweet['lang']+','+ tweet['timestamp_ms']\
-                               + ',' + str(tweet['retweet_count']) + '\n'
+                               + ',' + str(tweet['retweet_count'])+','+dia+','+hora+','+esdia+','+entresemana+','+ '\n'
                     tw_file_no_text.write(line_noText)
 
             except ValueError:
                 print 'Decoding JSON has failed'
     tw_file.close()
 
-def normalization(text, lang):
+def normalization(text):
     text = text.lower()
     other = ''
     hobj_spanish = hunspell.HunSpell('dic_spanish/Spanish.dic', 'dic_spanish/Spanish.aff')
@@ -72,23 +82,82 @@ def normalization(text, lang):
     words = re.findall(r'\w+', text, flags=re.UNICODE | re.LOCALE)
     depured = ""
     for word in words:
-        #Depuración de acuerdo al idioma
-        if lang == "en":
-            if word not in stopwords.words('english'):
-                word = stemmer_english.stem(word)
-                depured = depured+"&"+word
-        else:
-            if word not in stopwords.words('spanish'):
-                stem = hobj_spanish.stem(word)
-                if stem:
-                    word = stem[0]
-                depured = depured + "&" + word
-
+        word = deleteConsecutives(word)
+        #Won't pass if word lenght is lower than 2
+        if (len(re.findall('[a-zA-Z]', word))>2):
+            if (word != 'quito' or word != 'ecuador'):
+                alt1 = word
+                alt2 = word
+                flag = False
+                if alt1 not in stopwords.words('english'):
+                    alt1 = stemmer_english.stem(word)
+                else:
+                    alt1 = ''
+                if alt2 not in stopwords.words('spanish'):
+                    stem = hobj_spanish.stem(alt2)
+                    if stem:
+                        alt2 = stem[0]
+                        flag = True
+                else:
+                    alt2 = ''
+                #Won't pass if word is stopword on any lang
+                if (alt1!= '' and alt2 != ''):
+                    if (flag):
+                        depured = depured + "&" + alt2
+                    else:
+                        depured = depured + "&" + alt1
     return depured
 
 def remove_accents(input_str):
     nkfd_form = unicodedata.normalize('NFKD', unicode(input_str))
     return u"".join([c for c in nkfd_form if not unicodedata.combining(c)])
 
+
+def isDay(tiempo):
+    fecha = datetime.datetime.fromtimestamp(tiempo/1000.0)
+    hora = fecha.hour
+    #Entre 7 am y antes de las 18 pm
+    if (hora > 7 and hora < 18):
+        return '1'
+    else:
+        return '0'
+
+def isWeekDay(tiempo):
+    fecha = datetime.datetime.fromtimestamp(tiempo / 1000.0)
+    dia = fecha.weekday()
+    #Lunes, martes, miercoles y jueves
+    if (dia >= 0 and dia <=3):
+        return '1'
+    else:
+        return '0'
+
+def day(tiempo):
+    fecha = datetime.datetime.fromtimestamp(tiempo / 1000.0)
+    return  fecha.weekday()
+
+def hour(tiempo):
+    fecha = datetime.datetime.fromtimestamp(tiempo / 1000.0)
+    return fecha.hour()
+
+def deleteConsecutives(word):
+    if re.search(r'(.)\1\1', word):
+        prev1 = ''
+        prev2 = ''
+        result = ''
+        for ch in word:
+            if (ch != prev1 and ch != prev2):
+                if (prev2 != ''):
+                    prev2 = prev1
+                    prev1 = ch
+                    result=result+ch
+                else:
+                    prev1 = ch
+                    result = result + ch
+        return result
+    else:
+        return word
+
+
 if __name__ == '__main__':
     get_fields_tw()
+
