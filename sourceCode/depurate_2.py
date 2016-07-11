9e89a252b8790ce7dfe451ef96519c375133ce30
mvelasco93 <m_velasco93@hotmail.com>
2016-07-10 22:23:37 -0500

    added stopwords
    added more stopwords to depurate tweet and removal of unnecessary files

---
+++ sourceCode/depurate_2.py
@@ -0,0 +1,29 @@
+__author__ = 'madevelasco'
+
+import pandas as pd
+
+#IntinanMuseum
+#LaVirginlPanecillo
+#MuseoMindae
+#MuseoSanAgustin
+#ParqueLaCarolina
+
+place = "ParqueLaCarolina"
+path_open = "../data/Twitter/tw_cosas_hacer_"+place+".csv"
+path_result = "../data/Twitter/"+place+".csv"
+
+result = pd.read_csv(path_open, sep=',')
+local_stopwords = ['quito', 'ecuador', 'pichincha', 'quitoecuador', 'ecuadorquito', '', 'post', 'photo']
+
+def removestop(x):
+    words = x.split('&')
+    res = ''
+    for word in words:
+        if word not in local_stopwords:
+            res = res + "&"+ word
+    return res
+
+result['dep'] = result['words'].apply(removestop)
+result.
+
+result.to_csv(path_result, sep=',', encoding='utf-8')
