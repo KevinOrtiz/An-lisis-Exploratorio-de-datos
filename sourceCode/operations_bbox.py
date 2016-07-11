a555397e64def1e406b63e66513142b35c70f7cd
Cesar San Lucas <slace.93@gmail.com>
2016-07-10 22:24:20 -0500

    Descriptive statistics on twitter dataset

--- sourceCode/operations_bbox.py
+++ sourceCode/operations_bbox.py
@@ -1,7 +1,6 @@
 __author__ = 'cesar17'
 from geolocation_bbox import GeoLocation
 import pandas as pd
-from wordcloud import WordCloud
 import re
 
 def is_in_bounding_box(location, bbox):
@@ -37,12 +36,11 @@ def set_bbox_top_cosas_hacer():
         maxlong = NE_loc.deg_lon
         maxlat = NE_loc.deg_lat
         bboxes[key] = [minlong, minlat, maxlong, maxlat]
-    tweets_df = pd.read_csv("../data/Twitter/tweets_depurated.csv")
+    tweets_df = pd.read_csv("../data/Twitter/tweets_depurated.csv", index_col=False)
     tw_aux = tweets_df[['longitude','latitude']]
     tweets_df['bbox_cosas_hacer'] = tw_aux.apply(lambda x: get_index_bbox(x, bboxes), axis=1)
     poi_cosas_hacer_df = pd.DataFrame(columns=('poi_name', 'n_tw'))
     index_poi = 0
-
     for key in bboxes.iterkeys():
         tw_cosas_hacer_lugar = tweets_df[tweets_df['bbox_cosas_hacer']==key]
         size = len(tw_cosas_hacer_lugar.index)
@@ -57,6 +55,7 @@ def set_bbox_top_cosas_hacer():
     top_poi_cosas_hacer = poi_cosas_hacer_df.head(5)
     print top_poi_cosas_hacer
     poi_cosas_hacer_df.to_csv('../data/Twitter/poi_cosas_hacer_nTw.csv', index=False)
+    tweets_df.to_csv('../data/Twitter/tweets_depurated_bboxes.csv', index=False)
 
 def get_pois_location_trip_advisor(category_csv_file):
     sitios = {}
@@ -74,45 +73,6 @@ def get_pois_location_trip_advisor(category_csv_file):
         sitios[name] = [longitude, latitude]
     return sitios
 
-def get_wordCloud(frompoi_file_csv):
-    tw_cat_a = open(frompoi_file_csv)
-    #tw_cat_a = open('../data/Twitter/tw_cosas_hacer_a.csv')
-    text = ''
-    hashtags = ''
-    for line in tw_cat_a:
-        line = line.strip()
-        aux = line.split(',')
-        if aux[4] == 'es':
-            words = aux[3].split('&')
-            htags = aux[7].split('&')
-            for w in words:
-                if len(w) >= 3:
-                    text = text + w + ' '
-            text = text + '\n'
-            for h in htags:
-                if h != '-':
-                    hashtags = hashtags + h + ' '
-            # hashtags = hashtags + '\n'
-
-    print hashtags
-    # Generate a word cloud image
-    wordcloud = WordCloud().generate(text)
-
-    # Display the generated image:
-    # the matplotlib way:
-    import matplotlib.pyplot as plt
-    plt.imshow(wordcloud)
-    plt.title('Word Cloud Texto')
-    plt.axis("off")
-    # take relative word frequencies into account, lower max_font_size
-    # wordcloud = WordCloud(max_font_size=40, relative_scaling=.5).generate(ha)
-    wordcloud = WordCloud().generate(hashtags)
-    plt.figure()
-    plt.title('Word Cloud Hastags')
-    plt.imshow(wordcloud)
-    plt.axis("off")
-    plt.show()
 
 if __name__ == '__main__':
     set_bbox_top_cosas_hacer()
-    # get_wordCloud('../data/Twitter/tw_cosas_hacer_a.csv')
