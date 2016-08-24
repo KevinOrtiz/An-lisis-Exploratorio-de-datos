cc5e170be8dee8064b9308e5063f021b68479cb3
Cesar San Lucas <slace.93@gmail.com>
2016-08-24 10:18:41 -0500

    update code

--- sourceCode/graphics_descriptive_stats_tw.py
+++ sourceCode/graphics_descriptive_stats_tw.py
@@ -8,7 +8,7 @@ def descriptive_stats():
     tweets_df = pd.read_csv("../data/Twitter/tweets_depurated_bboxes.csv", index_col=False)
     poi_cosas_nTw =  pd.read_csv('../data/Twitter/poi_cosas_hacer_nTw.csv')
     size = str(len(tweets_df.index))
-    n_users = str(tweets_df['id_str'].nunique())
+    n_users = str(tweets_df['id_user_str'].nunique())
     print 'Total de tweets geolocalizados en el dataset:  ' + size
     print 'Total de usuarios unicos:   ' + n_users
     print '---'
@@ -16,7 +16,7 @@ def descriptive_stats():
     print 'Descripcion de tweets alrededor de los POIs'
     print poi_cosas_nTw['n_tw'].describe()
     tweets_df = tweets_df[tweets_df['bbox_cosas_hacer'] != '-']
-    gb_tweets_df = tweets_df[['id_str', 'isDay', 'isWeekDay', 'bbox_cosas_hacer']].groupby('bbox_cosas_hacer')
+    gb_tweets_df = tweets_df[['id_user_str', 'isDay', 'isWeekDay', 'bbox_cosas_hacer']].groupby('bbox_cosas_hacer')
     # Plot number of tweets
     nTw_poi = gb_tweets_df.size()
     nTw_poi = nTw_poi.sort_values(ascending=False).head(10)
@@ -28,7 +28,7 @@ def descriptive_stats():
     # plt.show()
     # ##
 
-    gb_uniq_user_tweets = gb_tweets_df['id_str'].apply(lambda x: len(x.unique()))
+    gb_uniq_user_tweets = gb_tweets_df['id_user_str'].apply(lambda x: len(x.unique()))
     gb_uniq_user_tweets = gb_uniq_user_tweets.sort_values(ascending=False).head(5)
     print '***** Number of twitter users at POIs *****'
     print gb_uniq_user_tweets
@@ -36,8 +36,6 @@ def descriptive_stats():
     my_plot.set_xlabel("Points of Interest")
     my_plot.set_ylabel("Number Users")
     plt.show()
-
-
 
 def get_wordCloud(from_poi_file_csv):
     local_stopwords = ['quito', 'ecuador', 'pichincha', 'quitoecuador', 'ecuadorquito', 'post', 'photo', 'dia', 'hoy', 'mas', 'uio']
@@ -87,6 +85,27 @@ def get_wordCloud(from_poi_file_csv):
     plt.show()
 
 
+def set_tripadvisor_helpfulvotes():
+    # 0 - 25 bajo
+    # 25 - 50 medio
+    # 50 alto
+    helpful_votes_ext = pd.read_csv("../data/TripAdvisor/helpfulextranjero.csv", index_col=False)
+    helpful_votes_ext['categoria'] = helpful_votes_ext['helph_vote'].apply(lambda x: set_rank_helpful_votes(x))
+    helpful_votes_ext.to_csv('../data/TripAdvisor/helpfulextranjero_categoria.csv', index=False)
+    helpful_votes_local = pd.read_csv("../data/TripAdvisor/datafinaLocalhelph.csv", index_col=False)
+    helpful_votes_local['categoria'] = helpful_votes_local['helph_vote'].apply(lambda x: set_rank_helpful_votes(x))
+    helpful_votes_local.to_csv('../data/TripAdvisor/datafinaLocalhelph_categoria.csv', index=False)
+
+
+def set_rank_helpful_votes(value):
+    if value >= 0 and value < 25:
+        return 'bajo'
+    elif value >= 25 and value < 50:
+        return 'medio'
+    else:
+        return 'alto'
+
+
 if __name__ == '__main__':
     descriptive_stats()
     file_rutas = ['../data/Twitter/tws_alrededor_poi/tw_cosas_hacer_MuseoMindae.csv', '../data/Twitter/tws_alrededor_poi/tw_cosas_hacer_IntinanMuseum.csv',
@@ -95,3 +114,4 @@ if __name__ == '__main__':
     # for ruta in file_rutas:
     #     get_wordCloud(ruta)
     # get_wordCloud('../data/Twitter/tws_alrededor_poi/tw_cosas_hacer_LaVirginlPanecillo.csv')
+    # set_tripadvisor_helpfulvotes()
