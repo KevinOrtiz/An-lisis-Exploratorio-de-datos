fd7f3856db61e3b7e1555dc5bea250e30ec84a64
josanvel <josanvel@espol.edu.ec>
2016-09-06 13:13:16 -0500

    User, location, name_lugar

--- sourceCode/scrapy_hotel/scrapy_hotel/ProcesamientoDeTexto/Documentos/user_location_place.py
+++ sourceCode/scrapy_hotel/scrapy_hotel/ProcesamientoDeTexto/Documentos/user_location_place.py
@@ -13,27 +13,34 @@ Estado:         Terminado
 """
 def create_user_location_hotels(input_file, output_file, dominio):
 
+	list_hotels = []
+	list_user_hotels = {}
+
 	with open(input_file) as dataFile: #File in  en Spanish
 			data_json = js.load(dataFile)
 
-	#Creacion del archivo CSV
-	csv_file = create_file('Hoteles/User_Location', dominio, 'csv')
-	cabecera_csv = 'User,Location,Place\n'			#Creo la cabecera del CSV
-	csv_file.write(cabecera_csv)
+	
+	with io.open('Hoteles/User_Location/'+dominio+'/user_location_'+dominio+'.json','w',encoding='utf-8') as json_file:
+		for element in data_json:
+			location = element['location_hotel']
+			if location[0] is not None and location[1] is not None:
+				place = element['name_hotel']
 
-	for element in data_json:
-		location = element['location_hotel']
-		if location[0] is not None and location[1] is not None:
-			place = element['name_hotel']
-
-			if element['reviews_hotel']:
-				for review in element['reviews_hotel']:
-					if review:
-						user = review['username_review']
-						if user:
-							line = str(user)+','+str(location)+','+str(place)+'\n'
-							csv_file.write(line)
-	csv_file.close()
+				if element['reviews_hotel']:
+					for review in element['reviews_hotel']:
+						if review:
+							user = review['username_review']
+							if user:
+								helpful = review['helpful_review']
+								list_user_hotels['username_review'] = user
+								list_user_hotels['location_hotel'] = location
+								list_user_hotels['name_hotel'] = place
+								list_user_hotels['helpful_review'] = helpful
+								
+					list_hotels.append(list_user_hotels)
+					list_user_hotels = {}
+		json_file.write(unicode(js.dumps(list_hotels, ensure_ascii=False)))
+	json_file.close()
 
 
 """
@@ -44,26 +51,33 @@ Estado:         Terminado
 """
 def create_user_location_activities(input_file, output_file, dominio):
 
-	with open(input_file) as dataFile: #File in  en Spanish
-			data_json = js.load(dataFile)
-	
-	#Creacion del archivo text
-	csv_file = create_file('Actividades/User_Location', dominio, 'csv')
-	cabecera_csv = 'User,Location,Place\n'			#Creo la cabecera del CSV
-	csv_file.write(cabecera_csv)			
+	list_activities = []
+	list_user_activities = {}
 
-	for element in data_json:
-		location = element['location_actividad']
-		if location[0] is not None and location[1] is not None:
-			place = element['name_actividad']
-			if element['reviews_actividad']:
-				for review in element['reviews_actividad']:
-					if review:
-						user = review['username_review']
-						if user:
-							line = str(user)+','+str(location)+','+str(place)+'\n'
-							csv_file.write(line)
-	csv_file.close()
+	with open(input_file) as dataFile: #File in  en Spanish
+			data_json = js.load(dataFile)		
+
+	with io.open('Actividades/User_Location/'+dominio+'/user_location_'+dominio+'.json','w',encoding='utf-8') as json_file:
+		for element in data_json:
+			location = element['location_actividad']
+			if location[0] is not None and location[1] is not None:
+				place = element['name_actividad']
+				if element['reviews_actividad']:
+					for review in element['reviews_actividad']:
+						if review:
+							user = review['username_review']
+							if user:
+								helpful = review['helpful_review']
+								list_user_activities['username_review'] = user
+								list_user_activities['location_actividad'] = location
+								list_user_activities['name_actividad'] = place
+								list_user_activities['helpful_review'] = helpful
+
+					list_activities.append(list_user_activities)
+					list_user_activities = {}
+		json_file.write(unicode(js.dumps(list_activities, ensure_ascii=False)))
+		json_file.write(unicode('\n'))
+	json_file.close()
 
 
 def create_file(folder,dominio, folder_extension):
